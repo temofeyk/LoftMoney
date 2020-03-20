d96d641d4fcc22219158445ae6293cb4903fe801
Artemy <temofey_k@mail.ru>
2020-03-20 21:28:32 +0300

    Ðadded hello world activity

--- app/src/main/java/com/temofey/k/android/loftmoney/activities/HelloWorldActivity.java
+++ app/src/main/java/com/temofey/k/android/loftmoney/activities/HelloWorldActivity.java
@@ -1,8 +1,10 @@
 package com.temofey.k.android.loftmoney.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.temofey.k.android.loftmoney.R;
 
@@ -11,6 +13,14 @@ public class HelloWorldActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hello_world);
+
+        Button btn = findViewById(R.id.btn_hello_world_start);
+        btn.setOnClickListener(v -> {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    R.string.hello_title, Toast.LENGTH_SHORT);
+            toast.show();
+        });
+
     }
 }
